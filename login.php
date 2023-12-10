f21a1f4508976b2809b771825b7c489f6b30bd93
Eduardo Bezerra <113864479+duutheboy@users.noreply.github.com>
2023-12-10 18:47:25 -0300

    tela de login

--- login.php
+++ login.php
@@ -8,7 +8,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
   </head>
-
   <div class="container">
     <div class="row">
         <div class="col-2">
@@ -36,11 +35,7 @@
                         <div class="text-center">
                             <button type="Submit" class="btn btn-success">Login</button>
                         </div>
-
-
                     </form>
-                    
-
                 </div>
             </div>
         </div>
